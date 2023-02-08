4db9112270cb32ccadf4e8eaf96b28885cfa47aa
dohun.yoon <dakies0355@gmail.com>
2023-02-09 03:11:31 +0900

    06.회원가입과 로그인 기능 개발하기(2)

--- src/test/java/com/fastcampus/snsproject/service/UserServiceTest.java
+++ src/test/java/com/fastcampus/snsproject/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.fastcampus.snsproject.service;
 
+import com.fastcampus.snsproject.exception.ErrorCode;
 import com.fastcampus.snsproject.exception.SnsApplicationException;
 import com.fastcampus.snsproject.fixture.UserEntityFixture;
 import com.fastcampus.snsproject.model.entity.UserEntity;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
 
@@ -25,6 +27,9 @@ public class UserServiceTest {
     @MockBean
     private UserEntityRepository userEntityRepository;
 
+    @MockBean
+    private BCryptPasswordEncoder encoder;
+
 
     @Test
     void 회원가입이_정상적으로_동작하는_경우(){
@@ -32,7 +37,8 @@ public class UserServiceTest {
         String password = "password";
 
         when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
-        when(userEntityRepository.save(any())).thenReturn(Optional.of(mock(UserEntity.class)));
+        when(encoder.encode(password)).thenReturn("encrypt_password");
+        when(userEntityRepository.save(any())).thenReturn(mock(UserEntity.class));
 
         Assertions.assertDoesNotThrow(() -> userService.join(userName, password));
     }
@@ -45,9 +51,11 @@ public class UserServiceTest {
         UserEntity fixture = UserEntityFixture.get(userName, password);
 
         when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
+        when(encoder.encode(password)).thenReturn("encrypt_password");
         when(userEntityRepository.save(any())).thenReturn(Optional.of(fixture));
 
-        Assertions.assertThrows(com.fastcampus.snsproject.exception.SnsApplicationException.class, () -> userService.join(userName, password));
+        SnsApplicationException e = Assertions.assertThrows(SnsApplicationException.class, () -> userService.join(userName, password));
+        Assertions.assertEquals(ErrorCode.DUPLICATED_USER_NAME, e.getErrorCode());
     }
 
     @Test
@@ -58,6 +66,7 @@ public class UserServiceTest {
         UserEntity fixture = UserEntityFixture.get(userName, password);
 
         when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
+        when(encoder.matches(password, fixture.getPassword())).thenReturn((true));
 
         Assertions.assertDoesNotThrow(() -> userService.login(userName, password));
     }
@@ -69,7 +78,8 @@ public class UserServiceTest {
 
         when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
 
-        Assertions.assertThrows(com.fastcampus.snsproject.exception.SnsApplicationException.class, () -> userService.login(userName, password));
+        SnsApplicationException e = Assertions.assertThrows(SnsApplicationException.class, () -> userService.login(userName, password));
+        Assertions.assertEquals(ErrorCode.USER_NOT_FOUND, e.getErrorCode());
     }
 
     @Test
@@ -80,8 +90,9 @@ public class UserServiceTest {
 
         UserEntity fixture = UserEntityFixture.get(userName, password);
 
-        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
 
-        Assertions.assertThrows(com.fastcampus.snsproject.exception.SnsApplicationException.class, () -> userService.login(userName, wrongPassword));
+        SnsApplicationException e = Assertions.assertThrows(SnsApplicationException.class, () -> userService.login(userName, wrongPassword));
+        Assertions.assertEquals(ErrorCode.INVALID_PASSWORD, e.getErrorCode());
     }
 }
